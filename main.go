c5f57342f0c6609e2c105a7d634b416060275805
Matti Jokitulppo <melonmanchan@gmail.com>
2017-10-28 17:14:18 +0300

    Add support for custom path and port

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,8 +15,15 @@ type HTTPStatus struct {
 	msg    string
 }
 
-func (h HTTPStatus) ToHeader() string {
-	return fmt.Sprintf("HTTP/1.1 %d %s\r\n", h.status, h.msg)
+var filetypes = map[string]string{
+	".html": "text/html",
+	".htm":  "text/html",
+	".js":   "application/javascript",
+	".css":  "text/css",
+	".png":  "image/png",
+	".jpg":  "image/jpeg",
+	".jpeg": "image/jpeg",
+	"":      "text/plain",
 }
 
 var notFound = HTTPStatus{404, "not found"}
@@ -23,12 +31,23 @@ var ok = HTTPStatus{200, "OK"}
 var methodNotAllowed = HTTPStatus{405, "method not allowed"}
 var serverError = HTTPStatus{500, "internal server error"}
 
+var port = flag.Int("port", 8081, "port to run under")
+var basePath = flag.String("path", ".", "base path")
+
+func (h HTTPStatus) ToHeader() string {
+	return fmt.Sprintf("HTTP/1.1 %d %s\r\n", h.status, h.msg)
+}
+
 func main() {
-	ln, err := net.Listen("tcp", ":8081")
+	flag.Parse()
+
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("Listening at port :%d", *port)
 
 	defer ln.Close()
 
@@ -41,17 +60,6 @@ func main() {
 
 		go handleConnection(conn)
 	}
-}
-
-var filetypes = map[string]string{
-	".html": "text/html",
-	".htm":  "text/html",
-	".js":   "application/javascript",
-	".css":  "text/css",
-	".png":  "image/png",
-	".jpg":  "image/jpeg",
-	".jpeg": "image/jpeg",
-	"":      "text/plain",
 }
 
 func getPathFromBytes(bytes []byte) (string, *HTTPStatus) {
@@ -67,7 +75,7 @@ func getPathFromBytes(bytes []byte) (string, *HTTPStatus) {
 }
 
 func safePath(reqPath string) string {
-	return path.Clean(strings.Replace(reqPath, "..", "", -1))
+	return "./" + path.Join(*basePath, strings.Replace(reqPath, "..", "", -1))
 }
 
 func readFile(reqPath string) ([]byte, error) {
@@ -100,7 +108,7 @@ func handleConnection(conn net.Conn) {
 
 	sanitizedPath := safePath(path)
 
-	dat, err := readFile("." + sanitizedPath)
+	dat, err := readFile(sanitizedPath)
 
 	if err != nil {
 		fmt.Fprint(conn, notFound.ToHeader())
