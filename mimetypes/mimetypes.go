22d2df1e0d74017f611c34b1734f0f14d374b1d2
Matti Jokitulppo <melonmanchan@gmail.com>
2017-11-15 16:18:51 +0200

    Add more mimetypes, ignore case for them

--- mimetypes/mimetypes.go
+++ mimetypes/mimetypes.go
@@ -1,19 +1,26 @@
 package mimetypes
 
-import "path"
+import (
+	"path"
+	"strings"
+)
 
 var filetypes = map[string]string{
 	".html": "text/html",
 	".htm":  "text/html",
+	"":      "text/plain",
 	".js":   "application/javascript",
+	".pdf":  "application/pdf",
 	".css":  "text/css",
 	".png":  "image/png",
 	".jpg":  "image/jpeg",
 	".jpeg": "image/jpeg",
-	"":      "text/plain",
+	".ttf	": "font/ttf",
+	".woff":  "font/woff",
+	".woff2": "font/woff2",
 }
 
 func GetContentType(fileName string) string {
-	name := path.Ext(fileName)
+	name := path.Ext(strings.ToLower(fileName))
 	return filetypes[name]
 }
